b043327f646df19130b2efa65b9db362a8b1a2bb
chrismadufor <chrismadufor@gmail.com>
2023-11-26 14:36:37 +0100

    menu and all

---
+++ index.js
@@ -0,0 +1,3 @@
+let progress = document.getElementById("progress");
+progress.style.background = `linear-gradient(to right, #1a1a1a 0%, #1a1a1a ${progress.value}%, #e3e3e3 ${progress.value}%, #e3e3e3 100%)`;
+
