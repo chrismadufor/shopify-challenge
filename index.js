9cc67ed54f1ebd0edc15f7515416add0378da038
Christian Madufor <chrismadufor@gmail.com>
2023-11-27 07:22:37 +0100

    started js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// Range
 let progress = document.getElementById("progress");
+
 progress.style.background = `linear-gradient(to right, #1a1a1a 0%, #1a1a1a ${progress.value}%, #e3e3e3 ${progress.value}%, #e3e3e3 100%)`;
 
+// menu
+let menu = document.getElementById("menu-pop-up");
+let menuBtn = document.getElementById("menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    // console.log()
+  if (menu.style.display === "none") menu.style.display = "block";
+  else menu.style.display = "none";
+});
+
+// alert
+let alert = document.getElementById("alert-pop-up");
+let alertBtn = document.getElementById("alert-btn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.style.display === "none") menu.style.display = "block";
+  else menu.style.display = "none";
+});
