e509838ec162079b89f2ef6a01d07092fb0a0381
chrismadufor <chrismadufor@gmail.com>
2023-11-27 10:10:55 +0100

    added js

--- index.js
+++ index.js
@@ -3,21 +3,41 @@ let progress = document.getElementById("progress");
 
 progress.style.background = `linear-gradient(to right, #1a1a1a 0%, #1a1a1a ${progress.value}%, #e3e3e3 ${progress.value}%, #e3e3e3 100%)`;
 
-// menu
+// menu & alert
 let menu = document.getElementById("menu-pop-up");
 let menuBtn = document.getElementById("menu-btn");
-
-menuBtn.addEventListener("click", () => {
-    // console.log()
-  if (menu.style.display === "none") menu.style.display = "block";
-  else menu.style.display = "none";
-});
-
-// alert
 let alert = document.getElementById("alert-pop-up");
 let alertBtn = document.getElementById("alert-btn");
+console.log("Flex", menu.style.display);
 
 menuBtn.addEventListener("click", () => {
-  if (menu.style.display === "none") menu.style.display = "block";
-  else menu.style.display = "none";
+  if (menu.style.display === "none" || menu.style.display === "") {
+    alert.style.display = "none";
+    menu.style.display = "flex";
+  } else menu.style.display = "none";
 });
+
+
+alertBtn.addEventListener("click", () => {
+  if (alert.style.display === "none" || alert.style.display === "") {
+    menu.style.display = "none";
+    alert.style.display = "flex";
+  } else alert.style.display = "none";
+});
+
+// trial
+let trialCloseIcon = document.getElementById("trial-icon")
+let trial = document.getElementById("trial")
+
+trialCloseIcon.addEventListener("click", () => {
+    // trial.style.visibility = "hidden"
+    trial.style.display = "none"
+})
+
+// set up
+let setUpWrap = document.getElementById("set-up-wrap")
+let setUpControlIcon = document.getElementById("setup-control-icon")
+
+setUpControlIcon.addEventListener("click", () => {
+    setUpWrap.classList.toggle("active")
+})
